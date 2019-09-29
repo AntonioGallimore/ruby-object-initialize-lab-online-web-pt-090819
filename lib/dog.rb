d05a3f3afe283d63b9b61be87e443cda5e337199
Antonio Gallimore <antonio.gallimore@gmail.com>
2019-09-29 13:56:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,6 @@
-class Dog 
- def initialize=(name)
-    @name=name
-end
-def name
-  @name
-end
-
-def breed=(breed)
-  @breed=breed
-end
-def breed
-  @breed
-end
+class Dog
+  def initialize(name, breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
 end
